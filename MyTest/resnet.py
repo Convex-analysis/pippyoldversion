3b0cb8ff224d77432c9887f3f70eca00b09618fd
Convex-analysis <799447128@qq.com>
2024-11-30 02:19:07 -0800

    Modified memfuser based on pippy0.1.0, It can be staged but hard to say if can be forwarded

---
+++ MyTest/resnet.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        
+        self.layers = nn.ModuleList()
+        planes = [64, 128, 256, 512]
+        strides = [1, 2, 2, 2]
+        for i in range(4):
+            layer = self._make_layer(block, planes[i], num_blocks[i], stride=strides[i])
+            setattr(self, f'layer{i}', layer)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        for layer in self.layers:
+            out = layer(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
---
+++ PippyUsage.py
@@ -0,0 +1,54 @@
+import torch
+from pippy.IR import Pipe, annotate_split_points, PipeSplitWrapper
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.data import (
+    create_dataset,
+    create_loader,
+    resolve_data_config,
+    Mixup,
+    FastCollateMixup,
+    AugMixDataset,
+)
+from timm.data import create_carla_dataset, create_carla_loader
+from timm.models import (
+    create_model,
+    safe_model_name,
+    resume_checkpoint,
+    load_checkpoint,
+    convert_splitbn_model,
+    model_parameters,
+)
+from timm.utils import *
+from timm.loss import (
+    LabelSmoothingCrossEntropy,
+    SoftTargetCrossEntropy,
+    JsdCrossEntropy,
+)
+from timm.optim import create_optimizer_v2, optimizer_kwargs
+from timm.scheduler import create_scheduler
+from timm.utils import ApexScaler, NativeScaler # Add this line to import create_model
+
+model = create_model(
+        "memfuser_baseline_e1d3",
+        pretrained=False,
+        drop_rate=0.0,
+        drop_connect_rate=None,  # DEPRECATED, use drop_path
+        drop_path_rate=0.1,
+        drop_block_rate=None,
+        global_pool=None,
+        bn_tf=False,
+        bn_momentum=None,
+        bn_eps=None,
+        scriptable=True,
+        checkpoint_path=None,
+        freeze_num=-1,
+    )
+
+annotate_split_points(model, {
+    'position_encoding': PipeSplitWrapper.SplitPoint.END,
+    'decoder': PipeSplitWrapper.SplitPoint.BEGINNING})
+pipe = Pipe.from_tracing(model)
+print(pipe)
