019cfb78d8f4917b68de9a841c80ada8e928c8a8
Convex-analysis <799447128@qq.com>
2024-12-02 01:27:49 -0800

    modified:   PippyUsage.py

--- PippyUsage.py
+++ PippyUsage.py
@@ -1,35 +1,16 @@
 import torch
 from pippy.IR import Pipe, annotate_split_points, PipeSplitWrapper
+from pippy.IR import LossWrapper
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from timm.data import (
-    create_dataset,
-    create_loader,
-    resolve_data_config,
-    Mixup,
-    FastCollateMixup,
-    AugMixDataset,
-)
-from timm.data import create_carla_dataset, create_carla_loader
+
 from timm.models import (
-    create_model,
-    safe_model_name,
-    resume_checkpoint,
-    load_checkpoint,
-    convert_splitbn_model,
-    model_parameters,
+    create_model
 )
 from timm.utils import *
-from timm.loss import (
-    LabelSmoothingCrossEntropy,
-    SoftTargetCrossEntropy,
-    JsdCrossEntropy,
-)
-from timm.optim import create_optimizer_v2, optimizer_kwargs
-from timm.scheduler import create_scheduler
-from timm.utils import ApexScaler, NativeScaler # Add this line to import create_model
+
 
 model = create_model(
         "memfuser_baseline_e1d3",
