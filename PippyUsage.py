8b44a46a6faff60b2a860c524f0f4a276dc2f2c7
convex-analysis <799447128@qq.com>
2025-01-07 11:01:32 -0800

    modified:   PippyUsage.py

--- PippyUsage.py
+++ PippyUsage.py
@@ -1,16 +1,34 @@
 import logging
 import time
+import os
+import sys
+import argparse
 from typing import OrderedDict
 import torch
+import resource, psutil
+from functools import reduce
+
 from pippy.IR import Pipe, annotate_split_points, PipeSplitWrapper
 from pippy.IR import LossWrapper
 from pippy.PipelineDriver import PipelineDriverFillDrain
 from pippy.microbatch import TensorChunkSpec
 from pippy.microbatch import LossReducer
+import pippy.fx
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from pippy import run_pippy
+from pippy.IR import MultiUseParameterConfig, Pipe, LossWrapper, PipeSplitWrapper, annotate_split_points
+from pippy.PipelineDriver import PipelineDriverFillDrain, PipelineDriver1F1B, PipelineDriverInterleaved1F1B, \
+    PipelineDriverBase
+from pippy.events import EventsContext
+from pippy.microbatch import sum_reducer, TensorChunkSpec
+from pippy.visualizer import events_to_json
+
+
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 from timm.utils import *
 from timm.optim import create_optimizer_v2, optimizer_kwargs
@@ -24,6 +42,26 @@ from timm.models import (
     convert_splitbn_model,
     model_parameters,
 )
+
+PROFILING_ENABLED = True
+CHECK_NUMERIC_EQUIVALENCE = True
+
+NUM_ITERATION = 100
+
+schedules = {
+    'FillDrain': PipelineDriverFillDrain,
+    '1F1B': PipelineDriver1F1B,
+    'Interleaved1F1B': PipelineDriverInterleaved1F1B,
+}
+
+pippy.fx.Tracer.proxy_buffer_attributes = True
+
+USE_TQDM = bool(int(os.getenv('USE_TQDM', '1')))
+
+def log_memory_usage(stage):
+    process = psutil.Process(os.getpid())
+    print(f"[{stage}] Memory usage: {process.memory_info().rss / 1024 / 1024} MB")
+
 
 class LAVLoss(nn.Module):
     def __init__(self):
@@ -304,33 +342,7 @@ def main():
         lr_scheduler, total_iters=num_epochs
     )
     
-    #initalize the dataloader
-    dataset_train = create_carla_dataset(
-            args.dataset,
-            root=args.data_dir,
-            towns=args.train_towns,
-            weathers=args.train_weathers,
-            batch_size=args.batch_size,
-            with_lidar=args.with_lidar,
-            with_seg=args.with_seg,
-            with_depth=args.with_depth,
-            multi_view=args.multi_view,
-            augment_prob=args.augment_prob,
-            temporal_frames=args.temporal_frames,
-        )
-    dataset_eval = create_carla_dataset(
-            args.dataset,
-            root=args.data_dir,
-            towns=args.val_towns,
-            weathers=args.val_weathers,
-            batch_size=args.batch_size,
-            with_lidar=args.with_lidar,
-            with_seg=args.with_seg,
-            with_depth=args.with_depth,
-            multi_view=args.multi_view,
-            augment_prob=args.augment_prob,
-            temporal_frames=args.temporal_frames,
-        )
+    
     
     NUM_ITERATIONs = 100
 
@@ -352,5 +364,163 @@ def main():
             mixup_fn=None,
         )
  '''   
+    
+def run_master(_, args):
+    MULTI_USE_PARAM_CONFIG = MultiUseParameterConfig.REPLICATE if args.replicate else MultiUseParameterConfig.TRANSMIT
+    print(f'REPLICATE config: {args.replicate} -> {MULTI_USE_PARAM_CONFIG}')
+    print("Using schedule:", args.schedule)
+    print("Using device:", args.device)
+    if args.rank == 0:
+        number_of_workers = 2
+        all_worker_ranks = list(range(0, number_of_workers))  # include master rank = 0
+        chunks = len(all_worker_ranks)
+        batch_size = args.batch_size * chunks
+
+
+        #initalize the dataloader
+        dataset_train = create_carla_dataset(
+            args.dataset,
+            root=args.data_dir,
+            towns=args.train_towns,
+            weathers=args.train_weathers,
+            batch_size=args.batch_size,
+            with_lidar=args.with_lidar,
+            with_seg=args.with_seg,
+            with_depth=args.with_depth,
+            multi_view=args.multi_view,
+            augment_prob=args.augment_prob,
+            temporal_frames=args.temporal_frames,
+        )
+        dataset_eval = create_carla_dataset(
+            args.dataset,
+            root=args.data_dir,
+            towns=args.val_towns,
+            weathers=args.val_weathers,
+            batch_size=args.batch_size,
+            with_lidar=args.with_lidar,
+            with_seg=args.with_seg,
+            with_depth=args.with_depth,
+            multi_view=args.multi_view,
+            augment_prob=args.augment_prob,
+            temporal_frames=args.temporal_frames,
+        )
+
+        class OutputLossWrapper(LossWrapper):
+            def __init__(self, module, loss_fn):
+                super().__init__(module, loss_fn)
+
+            @torch.autocast(device_type="cuda")
+            def forward(self, input, target):
+                output = self.module(input)
+                loss = self.loss_fn(output, target)
+                return {"output": output, "loss": loss}
+
+        log_memory_usage("Before initializing model")
+
+        model = create_model(
+            "memfuser_baseline_e1d3",
+            pretrained=False,
+            drop_rate=0.0,
+            drop_connect_rate=None,
+            drop_path_rate=0.1,
+            drop_block_rate=None,
+            global_pool=None,
+            bn_tf=False,
+            bn_momentum=None,
+            bn_eps=None,
+            scriptable=True,
+            checkpoint_path=None,
+            freeze_num=-1,
+        )
+
+        log_memory_usage("After initializing model")
+
+        annotate_split_points(model, {
+            'encoder': PipeSplitWrapper.SplitPoint.BEGINNING,
+            'decoder': PipeSplitWrapper.SplitPoint.BEGINNING
+        })
+
+        wrapper = OutputLossWrapper(model, MemFuserLoss())
+
+        pipe = Pipe.from_tracing(wrapper, MULTI_USE_PARAM_CONFIG)
+        pipe.to(args.device)
+
+        log_memory_usage("After creating Pipe")
+
+        output_chunk_spec = (TensorChunkSpec(0), sum_reducer)
+        process = psutil.Process(os.getpid())
+        print(f"Memory usage before pipelineDriver: {process.memory_info().rss / 1024 / 1024} MB")
+        pipe_driver: PipelineDriverBase = schedules[args.schedule](pipe, chunks,
+                                                                len(all_worker_ranks),
+                                                                all_ranks=all_worker_ranks,
+                                                                output_chunk_spec=output_chunk_spec,
+                                                                _record_mem_dumps=bool(args.record_mem_dumps),
+                                                                checkpoint=bool(args.checkpoint))
+
+        optimizer = pipe_driver.instantiate_optimizer(optim.Adam, lr=1e-3, betas=(0.9, 0.999), eps=1e-8)
+        lr_scheduler, num_epochs = create_scheduler(args, optimizer)
+        lr_scheduler_pipe = pipe_driver.instantiate_lr_scheduler(
+            lr_scheduler, total_iters=num_epochs
+        )
+        
+        log_memory_usage("After creating optimizer")
+        print(f"Memory usage after pipelineDriver: {process.memory_info().rss / 1024 / 1024} MB")
+        
+        this_file_name = os.path.splitext(os.path.basename(__file__))[0]
+        pipe_visualized_filename = f"{this_file_name}_visualized_{args.rank}.json"
+        batches_events_contexts = []
+        
+        for i in range(NUM_ITERATION):
+            train_one_epoch_pipeline(
+                i,
+                pipe_driver,
+                dataset_train,
+                optimizer,
+                MemFuserLoss(),
+                args,
+                writer=None,
+                lr_scheduler=lr_scheduler_pipe,
+                saver=None,
+                output_dir=None,
+                amp_autocast=None,
+                loss_scaler=None,
+                model_ema=None,
+                mixup_fn=None,
+            )
+
+        if args.visualize:
+            all_events_contexts: EventsContext = reduce(lambda c1, c2: EventsContext().update(c1).update(c2),
+                                                        batches_events_contexts, EventsContext())
+            with open(pipe_visualized_filename, "w") as f:
+                f.write(events_to_json(all_events_contexts))
+            print(f"Saved {pipe_visualized_filename}")
+        print('Finished')
+        log_memory_usage("End of training")
+        print(f"Communication overload: {pipe_driver.get_communication_overload()}")
+        print(f"Data transferred: {pipe_driver.get_data_transferred_mb()} MB")
+
+    else:
+        print("This is a worker rank")
+        print(f"Current Memory usage in this node is : {process.memory_info().rss / 1024 / 1024} MB")
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--world_size', type=int, default=int(os.getenv("WORLD_SIZE", 3)))
+    parser.add_argument('--rank', type=int, default=int(os.getenv("RANK", -1)))
+    parser.add_argument('--master_addr', type=str, default=os.getenv('MASTER_ADDR', 'localhost'))
+    parser.add_argument('--master_port', type=str, default=os.getenv('MASTER_PORT', '29500'))
+
+    parser.add_argument('--max_epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=4)
+
+    parser.add_argument('-s', '--schedule', type=str, default=list(schedules.keys())[0], choices=schedules.keys())
+    parser.add_argument('--replicate', type=int, default=int(os.getenv("REPLICATE", '0')))
+    parser.add_argument('--cuda', type=int, default=int(torch.cuda.is_available()))
+    parser.add_argument('--visualize', type=int, default=0, choices=[0, 1])
+    parser.add_argument('--record_mem_dumps', type=int, default=0, choices=[0, 1])
+    parser.add_argument('--num_worker_threads', type=int, default=16)
+    parser.add_argument('--checkpoint', type=int, default=0, choices=[0, 1])
+    args = parser.parse_args()
+    
+    print(torch.cuda.is_available())
+    run_pippy(run_master, args)
